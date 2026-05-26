0c93e95855f0e84731316e14251d7b9ea50d68d0
mkhussein91 <48103486+mkhussein91@users.noreply.github.com>
2026-05-26 15:16:13 -0400

    Update status.js

--- api/whoop/status.js
+++ api/whoop/status.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
 const token = (req.headers.cookie||"").match(/wa=([^;]+)/)?.[1];
 res.json({ connected: !!token });
 }
